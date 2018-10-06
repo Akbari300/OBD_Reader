650a11f2d0d8dff70f7d220f77ae1f9f3c3d6405
Ali Akbari <aliakbari@Alis-MacBook-Pro.local>
2018-10-06 11:30:03 +0300

    Added Login functionality and it's working before making laucher activity

---
+++ src/main/java/com/github/pires/obd/reader/activity/Loginpage.java
@@ -0,0 +1,43 @@
+package com.github.pires.obd.reader.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.github.pires.obd.reader.R;
+
+public class Loginpage extends AppCompatActivity {
+
+    EditText _txtUserName;
+    EditText _txtPassword;
+
+    TextView _txtRegister;
+    CardView _cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+
+
+        _txtUserName = findViewById(R.id.txtUrname);
+        _txtPassword = findViewById(R.id.txtPassword);
+        _txtRegister = findViewById(R.id.txtregister);
+        _cardView = findViewById(R.id.btnlogin);
+
+
+        _cardView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Loginpage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
